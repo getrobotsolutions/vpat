6e0dfcfe88c357fcb4d5311ddd47f5cf7836f11a
Robot Solution <manage.ars1@gmail.com>
2019-10-31 14:43:46 -0400

    bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -12,36 +12,36 @@
         switch (String.fromCharCode(keynum)) {
 
             case '1':
+                document.body.style.backgroundImage = "url('assets/suit.png')";
                 PlaySpeech("Thanks, John! I’m happy to be here.");
-                document.body.style.backgroundImage = "url('../assets/suit.png')";
                 break;
             case '2':
+                document.body.style.backgroundImage = "url('assets/suit.png')";
                 PlaySpeech("I can dance.");
-                document.body.style.backgroundImage = "url('../assets/suit.png')";
                 break;
             case '3':
+                document.body.style.backgroundImage = "url('assets/3.png')";
                 PlaySpeech("Safety is our number one priority.  We constantly look for all types of hazards! (large projection screen and robot torso screen flashes image of The Dukes of Hazzard.");
-                document.body.style.backgroundImage = "url('../assets/3.png')";
                 break;
             case '4':
+                document.body.style.backgroundImage = "url('assets/3.png')";
                 PlaySpeech("Funny you should ask…I have a program for that.  Here are the top ideas I generated for WestRock.");
-                document.body.style.backgroundImage = "url('../assets/3.png')";
                 break;
             case '5':
+                document.body.style.backgroundImage = "url('assets/5.png')";
                 PlaySpeech("Knitted Beard Hat.");
-                document.body.style.backgroundImage = "url('../assets/5.png')";
                 break;
             case '6':
+                document.body.style.backgroundImage = "url('assets/6.png')";
                 PlaySpeech("Quack: A Duck-Billed Protective Muzzle For Dogs.");
-                document.body.style.backgroundImage = "url('../assets/6.png')";
                 break;
             case '7':
+                document.body.style.backgroundImage = "url('assets/7.png')";
                 PlaySpeech("Baby Mop.");
-                document.body.style.backgroundImage = "url('../assets/7.png')";
                 break;
             case '8':
+                document.body.style.backgroundImage = "url('assets/7.png')";
                 PlaySpeech("I’m So glad you asked, John.  Productivity is my passion.  Robots perform much better than humans in nearly every respect, as you have seen.");
-                document.body.style.backgroundImage = "url('../assets/7.png')";
                 break;
             default:
                 alert("Try Again: " + String.fromCharCode(keynum));
