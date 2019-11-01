c0fb9d55f230d3315be02bcc77d9627031b32941
Robot Solution <manage.ars1@gmail.com>
2019-11-01 14:47:24 -0400

    bug fix

--- scripts/main.js
+++ scripts/main.js
@@ -51,7 +51,7 @@ var test;
                 GetCaptureImage();
                 GetCaptureImageBase64();
                 MakeRobotFace();
-                //SetRobotFace();
+                SetRobotFace();
                 var i=0;
                 test = setInterval(function () {
                     if(i===0)
